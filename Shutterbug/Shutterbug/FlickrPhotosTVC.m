e50b5aae69b5c652a87ac9154163ac45d11e11a0
joern <Gerrit@kaiMac.local>
2016-03-14 17:08:52 +0100

    - select image from TableView, show image in ImageView

--- Shutterbug/Shutterbug/FlickrPhotosTVC.m
+++ Shutterbug/Shutterbug/FlickrPhotosTVC.m
@@ -8,6 +8,7 @@
 
 #import "FlickrPhotosTVC.h"
 #import "FlickrFetcher.h"
+#import "ImageViewController.h"
 
 @interface FlickrPhotosTVC ()
 
@@ -60,48 +61,36 @@
 }
 
 
-/*
-// Override to support conditional editing of the table view.
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
-}
-*/
-
-/*
-// Override to support editing the table view.
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        // Delete the row from the data source
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-    }   
-}
-*/
-
-/*
-// Override to support rearranging the table view.
-- (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
-}
-*/
-
-/*
-// Override to support conditional rearranging of the table view.
-- (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the item to be re-orderable.
-    return YES;
-}
-*/
-
-/*
 #pragma mark - Navigation
+
+- (void)prepareImageViewController:(ImageViewController *)ivc toDisplayPhoto:(NSDictionary *)photo{
+    ivc.imageURL = [FlickrFetcher URLforPhoto:photo format:FlickrPhotoFormatLarge];
+    ivc.title = [photo valueForKeyPath:FLICKR_PHOTO_TITLE];
+}
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    if([sender isKindOfClass:[UITableViewCell class]]){
+        
+        NSIndexPath *indexPath = [self.tableView indexPathForCell:sender];
+        if(indexPath){
+            
+            if([segue.identifier isEqualToString:@"Display Photo"]){
+                if([segue.destinationViewController isKindOfClass:[ImageViewController class]]){
+                    
+                    [self prepareImageViewController:segue.destinationViewController toDisplayPhoto:self.photos[indexPath.row]];
+                }
+                
+            }
+            
+        }
+        
+    }
+    
 }
-*/
+
 
 @end
---
+++ Shutterbug/Shutterbug/ImageViewController.h
@@ -0,0 +1,15 @@
+//
+//  ImageViewController.h
+//  Imaginarium
+//
+//  Created by iOS Entwickler on 09.03.16.
+//  Copyright (c) 2016 noorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ImageViewController : UIViewController
+
+@property (nonatomic, strong) NSURL *imageURL;
+
+@end
