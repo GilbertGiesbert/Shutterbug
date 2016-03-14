252afe9896f0023da11747e0f0968eb06d40ed75
joern <Gerrit@kaiMac.local>
2016-03-14 12:48:06 +0100

    - added blank project

---
+++ Shutterbug/Shutterbug/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Shutterbug
+//
+//  Created by iOS Entwickler on 14.03.16.
+//  Copyright (c) 2016 noorg. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
